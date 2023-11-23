edb546bdc2f24c2b366561819b8f29e6a0f2e21d
Dmytro Polishchuk <60980897+do-ping@users.noreply.github.com>
2023-11-23 09:32:55 -0500

    Find array median with built-in sort

---
+++ find_the_median/main.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'findMedian' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def findMedian(arr):
+    return sorted(arr)[int(len(arr)/2)]
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    # n = int(input().strip())
+
+    # arr = list(map(int, input().rstrip().split()))
+
+    for arr in [
+        [0, 1, 2, 4, 6, 5, 3],  # 3
+        [99, 119, 666, 231, 453, 777, 878],  # 453
+        [11, 10, 9, 8, 7, 6, 5, 4, 3]  # 7
+    ]:
+        result = findMedian(arr)
+        print(arr)
+        print(result)
+
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()
