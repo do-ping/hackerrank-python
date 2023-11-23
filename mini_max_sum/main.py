f3c6069fbdf7afebd12cab37e89397c5b7344319
Dmytro Polishchuk <60980897+do-ping@users.noreply.github.com>
2023-11-23 08:29:59 -0500

    Mini-Max sum with sort and reduce

---
+++ mini_max_sum/main.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+from operator import add
+from functools import reduce
+
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    if not arr or len(arr) > 5:
+        return
+
+    arr = sorted(arr)
+    print(f"{reduce(add, arr[:-1])} {reduce(add, arr[1:])}")
+
+
+if __name__ == '__main__':
+
+    # arr = list(map(int, input().rstrip().split()))
+
+    for arr in [
+        [1, 3, 5, 7, 9],
+        [1, 2, 3, 4, 5],
+        [10000000, 20000000, 90000000, 50000000, 999999999]
+    ]:
+        miniMaxSum(arr)
